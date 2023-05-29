223353947e1ae038ab6accc191587609627d559d
nathanaeltchamba <natanshost@gmail.com>
2023-05-29 09:46:39 -0500

    about page html added

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Homepage
+from .views import Homepage, About
 
 # routes
 urlpatterns = [
     path('', Homepage.as_view(), name = 'home'),
+    path('about/', About.as_view(), name = 'about'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Homepage(TemplateView):
     template_name = 'homepage.html'
+
+class About(TemplateView):
+    template_name = 'about/about.html'
